8c524d32efa85e7d4a6f6b885377003c3143cec5
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-04-17 16:16:39 +0000

    Prueba de concepto-Corregir salida de nota maximo y minima

--- main.c
+++ main.c
@@ -1,3 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() { return 0; }
+int main() {
+  srand(time(NULL));
+
+  int i, j;
+  int suma = 0;
+  int max = 0;
+  int min = 0;
+
+  int matrizA[5][3] = {0};
+  // int res[5][3] = {0};
+
+  for (i = 0; i < 5; i++) {
+    for (j = 0; j < 3; j++) {
+      matrizA[i][j] = rand() % 10 + 1;
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    printf("Estudiante -> %d\t\n", i);
+  }
+
+  for (i = 0; i < 5; i++) {
+    printf("\n");
+    for (j = 0; j < 3; j++) {
+      printf("%d\t", matrizA[i][j]);
+    }
+  }
+  int res[5][3] = {0};
+
+  for (i = 0; i < 1; i++) {
+    printf("\n");
+    for (j = 0; j < 3; j++) {
+      // printf("Estudiante A  -> %d\t", matrizA[i][j]);
+      res[i][j] = matrizA[i][j];
+      suma += matrizA[i][j];
+
+      if (matrizA[0][0] > matrizA[0][1]) {
+        max = matrizA[0][0];
+      } else {
+        max = matrizA[0][1];
+      }
+
+      if (matrizA[0][1] > matrizA[0][2]) {
+        max = matrizA[0][1];
+      } else {
+        max = matrizA[0][2];
+      }
+
+      if (matrizA[0][0] < matrizA[0][1]) {
+        min = matrizA[0][0];
+      } else {
+        min = matrizA[0][1];
+      }
+
+      if (matrizA[0][1] < matrizA[0][2]) {
+        min = matrizA[0][1];
+      } else {
+        min = matrizA[0][2];
+      }
+    }
+  }
+  printf("---------Estudiantes----------\n");
+  printf("\nEstudiante A  -> %d\t%d\t%d", res[0][0], res[0][1], res[0][2]);
+  printf("\n Promedio -> %.2f", (float)suma / 3);
+  printf("\nNota mas alta -> %d\n", max);
+  printf("\nNota mas baja -> %d", min);
+
+  printf("\n---------Materias---------\n");
+}
