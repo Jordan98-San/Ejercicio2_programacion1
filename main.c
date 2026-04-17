ce77644bccf3c699bd9239d96fca5cec675ec05b
Jordan98-San <jordan.sanchez@udla.edu.ec>
2026-04-17 16:53:11 +0000

    Funciones para el calculo de la nota mas baja completada.Primera parte completada.

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -12,7 +11,18 @@ int max(int a, int b) {
   }
 }
 
-int maximo_tres(int a, int b, int c) { return max(max(a, b), max(b, c)); }
+int maxima_Nota(int a, int b, int c) { return max(max(a, b), max(b, c)); }
+
+int min(int a, int b) {
+  int notaMinima = a;
+  if (b < notaMinima) {
+    return b;
+  } else {
+    return notaMinima;
+  }
+}
+
+int minima_Nota(int a, int b, int c) { return min(min(a, b), min(b, c)); }
 
 int main() {
   srand(time(NULL));
@@ -20,7 +30,7 @@ int main() {
   int i, j;
   int suma = 0;
   int maximaNota = 0;
-  int min = 0;
+  int minimaNota = 0;
 
   int matrizA[5][3] = {0};
   // int res[5][3] = {0};
@@ -50,26 +60,15 @@ int main() {
       res[i][j] = matrizA[i][j];
       suma += matrizA[i][j];
 
-      maximaNota = maximo_tres(matrizA[0][0], matrizA[0][1], matrizA[0][2]);
-
-      if (matrizA[0][0] < matrizA[0][1]) {
-        min = matrizA[0][0];
-      } else {
-        min = matrizA[0][1];
-      }
-
-      if (matrizA[0][1] < matrizA[0][2]) {
-        min = matrizA[0][1];
-      } else {
-        min = matrizA[0][2];
-      }
+      maximaNota = maxima_Nota(matrizA[0][0], matrizA[0][1], matrizA[0][2]);
+      minimaNota = minima_Nota(matrizA[0][0], matrizA[0][1], matrizA[0][2]);
     }
   }
   printf("---------Estudiantes----------\n");
   printf("\nEstudiante A  -> %d\t%d\t%d", res[0][0], res[0][1], res[0][2]);
   printf("\n Promedio -> %.2f", (float)suma / 3);
   printf("\nNota mas alta -> %d\n", maximaNota);
-  printf("\nNota mas baja -> %d", min);
+  printf("\nNota mas baja -> %d", minimaNota);
 
   printf("\n---------Materias---------\n");
 }
